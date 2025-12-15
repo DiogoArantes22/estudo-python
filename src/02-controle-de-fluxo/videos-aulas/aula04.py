fc4c8f9a7bae0215e7d47488525a59ca352c76c7
Diogo Arantes <diogoarantes2202@gmail.com>
2025-12-14 22:27:12 -0300

    Aula04

---
+++ src/02-controle-de-fluxo/videos-aulas/aula04.py
@@ -0,0 +1,14 @@
+"""Aula 04 - while loop"""
+# while loop é usado para repetir um bloco de código enquanto uma condição for verdadeira
+
+contador = 0
+while contador <= 10:
+    print('Contador:', contador)
+    contador += 1  # Incrementa o contador em 1
+    
+print('Fim')
+
+"""linha = arquivo.readline()
+while linha:
+    print('Linha do arquivo:', linha)
+    linha = arquivo.readline()"""
